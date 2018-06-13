4d4cd56c916d520c79e40f0c5b23c830ff87792b
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2018-06-13 19:36:28 +0300

    Improve benchmakk script

--- bench/bench.py
+++ bench/bench.py
@@ -17,27 +17,41 @@ dictionary = (
 )
 
 create0 = """
-create table t0(a json);
+create table t0(a jsonb);
 alter table t0 alter column a set storage external;
 """
 
 create1 = """
-create table t1(a json compression pglz);
+create table t1(a jsonb compression pglz);
 """
 
 dict_str = ",".join(dictionary)
 create2 = """
 create extension pg_dict_compression;
-create table t2(a json compression pg_dict_compression with (dict '%s'));
+create table t2(a jsonb compression pg_dict_compression with (dict '%s'));
 """ % dict_str
 
 create3 = """
-create table t3(a json compression zlib with (level 'best_compression'));
+create table t3(a jsonb compression zlib with (level 'best_compression'));
+"""
+
+create4 = """
+create extension zson;
+create table t4(a zson);
+"""
+learn_sql = """
+select zson_learn('{{"t0", "a"}}');
+"""
+
+create5 = """
+create extension pg_zstd;
+create table t5(a jsonb compression pg_zstd with (level '3'));
 """
 
 insert_sql = """
 insert into {0} values ('%s')
 """
+
 
 class benchmark(object):
     def __init__(self, name, suit=None, suit_key=None):
@@ -65,6 +79,8 @@ with get_new_node("test1") as node:
     node.safe_psql(create1)
     node.safe_psql(create2)
     node.safe_psql(create3)
+    node.safe_psql(create4)
+    node.safe_psql(create5)
 
     with node.connect() as con:
         with benchmark("insert uncompressed"):
@@ -94,3 +110,18 @@ with get_new_node("test1") as node:
 
         print("t3 size")
         print(node.safe_psql("select pg_size_pretty(pg_total_relation_size('t3'))"))
+
+        con.execute(learn_sql)
+        with benchmark("insert zson compressed"):
+            for i in range(10000):
+                con.execute(insert_sql.format("t4") % doc)
+
+        print("t4 size")
+        print(node.safe_psql("select pg_size_pretty(pg_total_relation_size('t4'))"))
+
+        with benchmark("insert zstd compressed"):
+            for i in range(10000):
+                con.execute(insert_sql.format("t5") % doc)
+
+        print("t5 size")
+        print(node.safe_psql("select pg_size_pretty(pg_total_relation_size('t5'))"))
